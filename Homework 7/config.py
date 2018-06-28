1bcf7a09e21a2dc576d168957e7efea8af78be18
gcgilb <38143313+gcgilb@users.noreply.github.com>
2018-06-28 16:31:48 -0400

    Add files via upload

---
+++ Homework 7/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "u89RvxjLYaYWvlIGLxuGstLbW"
+consumer_secret = "0CSPceuZ4KtbSy8om26ErBnC02dFV8V4eTaZ7iOWEdD0uT6dOv"
+access_token = "28390355-4MdqKvQhoyMULUKyCZAwSY2YUWonw2DSzoN7KZotN"
+access_token_secret = "dTAADDNsjRH3EOBEFHOnvQAe7J7qG15bYYx8gqasEHjbN"
